005dcc05eadaa7df1f98063995dc5c912ce58571
R-Morgan <rmorgan@trystero.is>
2014-09-27 11:57:35 -0700

    First Commit

---
+++ script-01.py
@@ -0,0 +1,16 @@
+""" Before playing with this code, I'd *highly* recommend installing python 3
+and bpython, a very slick python interpreter that will make trying out the code
+very easy."""
+
+
+import nltk, re, pprint           # imports several modules for text processing
+
+from nltk import word_tokenize, sent_tokenize
+
+""" The command imports some specific nltk submodules to get the raw text into 
+a more useful form  """
+
+
+
+
+
